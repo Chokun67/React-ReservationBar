e8ae950376c60da51ce630b13dbb5eced5b040f6
Chokun <nobunaka1412@gmail.com>
2023-09-20 02:19:13 +0700

    add RegisterPage

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,140 @@
-import React from "react";
-import "../assets/style/login.css";
+import React, { useState } from "react";
+import "../assets/style/register.css";
+import logo from "../assets/image/logo.png";
+import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
 function Signup() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    username: "",
+    password: "",
+    telephone: "",
+    birthday: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // ส่งข้อมูลฟอร์มไปยังเซิร์ฟเวอร์หรือทำอย่างอื่นตามต้องการ
+    console.log(formData);
+  };
+
   return (
     <>
-      <div class="grid-container">
-        <div class="left-half">
-        <h1>Text register</h1>
+      <div className="flex-container">
+        <div className="re-left-half">
+          <div className="login-container">
+            <h1>HELLO THERE!</h1>
+            <p>
+              Please provide information to register your <br />
+              account before making reservations.
+              <br />
+              Thank you!
+            </p>
+            <div className="logo">
+              <img src={logo} alt="Your Logo" />
+            </div>
+            <p>Already have an account? Log in!</p>
+
+            <Link to="/login">
+              {" "}
+              <div className="go-register">
+                <p>Login</p>
+              </div>
+            </Link>
+          </div>
         </div>
-        <div class="right-half"></div>
+        <div className="re-right-half">
+          <div className="login-container">
+            <h1>Register</h1>
+            <form className="login-form">
+              <div className="name-container">
+                <div>
+                  <label>First Name:</label>
+                  <input
+                    type="text"
+                    name="firstName"
+                    value={formData.firstName}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div>
+                  <label>Last Name:</label>
+                  <input
+                    type="text"
+                    name="lastName"
+                    value={formData.lastName}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div>
+                <label>Email:</label>
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label>Username:</label>
+                <input
+                  type="text"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label>Password:</label>
+                <input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="name-container">
+                <div>
+                  <label>Telephone:</label>
+                  <input
+                    type="tel"
+                    name="telephone"
+                    value={formData.telephone}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div>
+                  <label>Birthday:</label>
+                  <input
+                    type="date"
+                    name="birthday"
+                    value={formData.birthday}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <button type="submit" className="login-button">
+                Submit
+              </button>
+              <div className="separator">
+                <hr />
+                <span>or</span>
+                <hr />
+              </div>
+              <button type="button" className="google-login-button">
+              <FcGoogle className="icon-google"/>Continue with Google
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "../assets/style/login.css";
-import logo from '../assets/image/logo.png';
-import { Link } from 'react-router-dom';
+import logo from "../assets/image/logo.png";
+import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
 
 function Signin() {
   return (
     <>
-      <div class="flex-container">
-        <div class="left-half">
+      <div className="flex-container">
+        <div className="left-half">
           <div className="login-container">
-          <h1>Login</h1>
+            <h1>Login</h1>
             <form className="login-form">
-            
               <label htmlFor="username">Username</label>
               <input type="text" id="username" name="username" required />
 
@@ -27,7 +27,7 @@ function Signin() {
                 <hr />
               </div>
               <button type="button" className="google-login-button">
-                Login with Google
+              <FcGoogle className="icon-google"/> Login with Google
               </button>
             </form>
           </div>
@@ -35,16 +35,23 @@ function Signin() {
         <div className="right-half">
           <div className="login-container">
             <h1>WELCOME BACK!</h1>
-            <p>Please log in to your account with your username <br/>and password before making reservations.<br/>Thank you!</p>
+            <p>
+              Please log in to your account with your username <br />
+              and password before making reservations.
+              <br />
+              Thank you!
+            </p>
             <div className="logo">
-            <img src={logo} alt="Your Logo" />
-          </div>
-          <p>Already have an account? Log in!</p>
-          <div className="go-register">
-            <Link to="/register"> Register</Link>
-          </div>
-             
-          
+              <img src={logo} alt="Your Logo" />
+            </div>
+            <p>Already have an account? Log in!</p>
+
+            <Link to="/register">
+              {" "}
+              <div className="go-register">
+              <p>Register</p>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
